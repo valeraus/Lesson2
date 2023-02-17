9619160900a1efb5d4a626635cc65285ea05306a
valeraus <vlaus0@gmail.com>
2023-02-17 17:57:33 +0200

    Виконав завдання 2

---
+++ Task2/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Converter
+    {
+        double usd, eur, rub;
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+        public void ToUsd(double uahSum)
+        {
+            Console.WriteLine(uahSum / usd);
+        }
+
+        public void FromUsd(double usdSum)
+        {
+            Console.WriteLine(usdSum * usd);
+        }
+
+        public void ToEur(double uahSum)
+        {
+            Console.WriteLine(uahSum / eur);
+        }
+
+        public void FromEur(double eurSum)
+        {
+            Console.WriteLine(eurSum * usd);
+        }
+
+        public void ToRub(double uahSum)
+        {
+            Console.WriteLine(uahSum / rub);
+        }
+
+        public void FromRub(double rubSum)
+        {
+            Console.WriteLine(rubSum * usd);
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Потрібно: Створити клас Converter. 
+    У тілі класу створити користувальницький конструктор, 
+    який приймає три речові аргументи, і ініціалізує поля, що відповідають курсу 3-х основних валют, 
+    по відношенню до гривні – public Converter (double usd, double eur, double rub). 
+    Написати програму, яка виконуватиме конвертацію з гривні в одну із зазначених валют, 
+    також програма повинна проводити конвертацію із зазначених валют у гривню. 
+     */
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Converter converter = new Converter(40, 41, 0.1);
+
+            converter.ToUsd(100);
+            converter.FromUsd(100);
+
+            Console.ReadKey();
         }
     }
 }
